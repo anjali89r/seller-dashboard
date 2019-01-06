fc8a13884e5df541a29ecc3f245def42a4cfbb0a
Anjali R <anjeng1989@gmail.com>
2019-01-06 13:31:14 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -20,11 +20,8 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(auth)
 Vue.use(VeeValidate);
 
-//Vue.use(VueResource);
-//Vue.http.options.root = "https://80uim6qgpb.execute-api.ap-south-1.amazonaws.com/dev/"
-//const base_URL = "https://80uim6qgpb.execute-api.ap-south-1.amazonaws.com/dev/"
-//const axios = require('axios')
-Vue.prototype.$baseURL = 'https://80uim6qgpb.execute-api.ap-south-1.amazonaws.com/dev/'
+'update url here
+Vue.prototype.$baseURL = ''
 Vue.config.productionTip = false
 
 new Vue({
@@ -32,7 +29,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-//  module.exports = {
-//    baseURL: 'https://80uim6qgpb.execute-api.ap-south-1.amazonaws.com/dev/'
-//  }
-//axios.defaults.baseURL = 'https://80uim6qgpb.execute-api.ap-south-1.amazonaws.com/dev/'
